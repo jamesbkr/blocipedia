bc8b072e47b8ba1f9318a6b061ae2ec6c5eb9cf1
James Becker <becker.james90@gmail.com>
2015-10-06 22:51:44 +0000

    fix user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :wikis
-  after_initialize :init 
   
   before_save { self.email = email.downcase}
  
@@ -10,13 +9,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
          
-  enum role: [:member,:admin, :premium]
+  enum role: [:member, :admin, :premium]
   
-  
-  
-  
-  def init
-      self.role = :member
-  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 20151005194201) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.integer  "role"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.integer  "role",                   default: 0
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
